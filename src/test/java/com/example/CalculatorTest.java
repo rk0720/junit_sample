3641fc0d6a0d2ff1d8c59d3d42a024390e5bb25e
rk0720 <ryotakatsumataax@gmail.com>
2024-01-09 19:41:42 +0900

    lesson4

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,14 +1,35 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
 	@Test
-	void test() {
-		fail("まだ実装されていません");
+	void multiplyで3と4の蒸散結果を取得() {
+		Calculator sut = new Calculator();	
+		int expected = 12;
+		int actual = sut.multiply(3, 4);
+		
+		assertThat(actual).isEqualTo(expected);
 	}
-
+	
+	@Test
+	void multiplyで5と6の乗算結果を取得() {
+		Calculator sut = new Calculator();
+		int excepted = 30;
+		int actual = sut.multiply(5, 6);
+		
+		assertThat(actual).isEqualTo(excepted);
+	}
+	
+	@Test
+	void devideで3と2の除算結果を取得() {
+		Calculator sut = new Calculator();
+		double excepted = 1.5;
+		double actual = sut.devide(3, 2);
+		
+		assertThat(actual).isEqualTo(excepted);
+	}
 }
