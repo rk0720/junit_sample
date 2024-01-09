5f03c44bd792418f6931f9ec0978e4e05a00aa70
rk0720 <ryotakatsumataax@gmail.com>
2024-01-09 19:16:39 +0900

    first commit

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
